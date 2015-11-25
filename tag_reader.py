b3c0edb1bc97731d5a279188f41b9cedb71e9823
Philip Hansen <me@philiphansen.dk>
2015-11-25 12:28:36 +0100

    Read tags in tags
    The tag list can now include tag lists

--- tag_reader.py
+++ tag_reader.py
@@ -1,6 +1,4 @@
-data = "00101001"
-
-tag_list = []
+data = "3430689156"
 
 def read_tag(data):
     tag = data[0]
@@ -8,8 +6,23 @@ def read_tag(data):
 
     return tag, remainder
 
-while len(data) > 0:
-    tag, data = read_tag(data)
-    tag_list.append(tag)
+def read_tags(data):
+    tag_list = []
 
-print tag_list
+    while len(data) > 0:
+        tag, data = read_tag(data)
+
+        if tag == "0":
+            print "compound"
+            new_tags, data = read_tags(data)
+            tag_list.append(new_tags)
+        elif tag == "1":
+            print "end"
+            return tag_list, data
+        else:
+            print tag
+            tag_list.append(tag)
+
+    return tag_list
+
+print read_tags(data)
