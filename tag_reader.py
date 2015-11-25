18de0ec1a578c3bbb81e0c851da2f9ddb0cc244b
Philip Hansen <me@philiphansen.dk>
2015-11-25 21:08:41 +0100

    Store tags as objects
    Tags are now stored inside a tag object, rather than as string variables

--- tag_reader.py
+++ tag_reader.py
@@ -1,22 +1,33 @@
 data = "30430436189156"
 
-def read_tag(data):
-    tag = data[0]
-    remainder = data[1:]
+class Tag(object):
+    def __init__(self, data):
+        self.data = data
 
-    return tag, remainder
+    def __str__(self):
+        return "Tag - '{}'".format(self.data)
+
+    def __repr__(self):
+        return "Tag - '{}'".format(self.data)
+
+    @classmethod
+    def get_tag_from_data(cls, data):
+        tag = Tag(data[0])
+        remainder = data[1:]
+
+        return tag, remainder
 
 def read_tags(data):
     tag_list = []
 
     while len(data) > 0:
-        tag, data = read_tag(data)
+        tag, data = Tag.get_tag_from_data(data)
 
-        if tag == "0":
+        if tag.data == "0":
             print "compound"
             new_tags, data = read_tags(data)
-            tag_list.append(new_tags)
-        elif tag == "1":
+            tag_list.append(Tag(new_tags))
+        elif tag.data == "1":
             print "end"
             return tag_list, data
         else:
